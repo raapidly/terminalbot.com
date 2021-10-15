62207fe5e6ba560412d5f49eb718d01a02deeb91
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-10-15 11:01:19 +0700

    Update factory.py

--- factory.py
+++ factory.py
@@ -1,9 +1,8 @@
-from flask import Flask
-
-from config import DevelopmentConfig as Config
-
-
 def create_app():
+    from flask import Flask
+    from config import DevelopmentConfig as Config
+
     app = Flask(import_name=__name__)
     app.config.from_object(obj=Config)
+
     return app
