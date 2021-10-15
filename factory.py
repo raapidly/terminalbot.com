b8acddaad680387a109b8050eb15fc1d17c7c2bf
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-10-15 14:21:35 +0700

    Update factory.py

--- factory.py
+++ factory.py
@@ -7,11 +7,12 @@ def create_app():
     from flask import Flask, send_from_directory
     from config import DevelopmentConfig as Config
 
+    app = Flask(import_name=__name__)
+    app.config.from_object(obj=Config)
+
     def favicon():
         return send_from_directory(directory='static', path='favicon.ico')
 
-    app = Flask(import_name=__name__)
-    app.config.from_object(obj=Config)
     app.add_url_rule(rule='/favicon.ico', view_func=favicon)
 
     return app
