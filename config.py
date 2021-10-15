4623640974459f84c5aad4d1374c38cd8ab2f1f7
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-10-15 11:04:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 class Config:
+    """
+    Base config.
+    """
     JSON_SORT_KEYS = False
 
 
 class DevelopmentConfig(Config):
+    """
+    Development config.
+    """
     pass
 
 
 class ProductionConfig(Config):
+    """
+    Production config.
+    """
     pass
