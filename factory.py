5b370f9401c00da71bca047b8d7453c17a583c88
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-10-26 09:48:26 +0700

    Refactor

--- factory.py
+++ factory.py
@@ -10,10 +10,46 @@ def create_app():
     app = Flask(import_name=__name__)
     app.config.from_object(obj=Config)
 
+    @app.route('/site.webmanifest', methods=['GET'])
+    def webmanifest():
+        return send_from_directory(
+            directory='assets',
+            path='site.webmanifest')
+
     @app.route('/favicon.ico', methods=['GET'])
     def favicon():
         return send_from_directory(
-            directory='static',
-            path='favicon.ico')
+            directory='assets',
+            path='img/favicon.ico')
+
+    @app.route('/favicon-16x16.png', methods=['GET'])
+    def favicon_16():
+        return send_from_directory(
+            directory='assets',
+            path='img/favicon-16x16.png')
+
+    @app.route('/favicon-32x32.png', methods=['GET'])
+    def favicon_32():
+        return send_from_directory(
+            directory='assets',
+            path='img/favicon-32x32.png')
+
+    @app.route('/android-chrome-192x192.png', methods=['GET'])
+    def favicon_android_chrome_192():
+        return send_from_directory(
+            directory='assets',
+            path='img/android-chrome-192x192.png')
+
+    @app.route('/android-chrome-512x512.png', methods=['GET'])
+    def favicon_android_chrome_512():
+        return send_from_directory(
+            directory='assets',
+            path='img/android-chrome-512x512.png')
+
+    @app.route('/apple-touch-icon.png', methods=['GET'])
+    def favicon_apple_touch_icon():
+        return send_from_directory(
+            directory='assets',
+            path='img/apple-touch-icon.png')
 
     return app
