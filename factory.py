421894d2ec95dab8aa9d0f4bd15de61c604373f3
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-10-26 11:31:31 +0700

    Add Flask-Minify

--- factory.py
+++ factory.py
@@ -5,9 +5,11 @@ def create_app():
     :return: Flask object
     """
     from flask import Flask
+    from flask_minify import minify
     from config import DevelopmentConfig as Config
 
     app = Flask(import_name=__name__)
     app.config.from_object(obj=Config)
+    minify(app=app)
 
     return app
