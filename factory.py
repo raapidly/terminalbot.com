91ab7967aae7bc64eb9d4a690fd6e289bd667c3d
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-10-29 15:31:27 +0700

    Flask Session

--- factory.py
+++ factory.py
@@ -7,9 +7,18 @@ def create_app():
     from flask import Flask
     from flask_minify import minify
     from flask_session import Session
+    from redis import StrictRedis
 
     app = Flask(import_name=__name__)
     app.config.from_json(filename='config.json')
+    app.config.update({
+        'SESSION_REDIS': StrictRedis(
+            host=app.config.get('SESSION_REDIS_HOST'),
+            port=app.config.get('SESSION_REDIS_PORT'),
+            db=app.config.get('SESSION_REDIS_DB'),
+            password=app.config.get('SESSION_REDIS_PASSWORD'))
+    })
+
     Session(app=app)
     minify(
         app=app,
