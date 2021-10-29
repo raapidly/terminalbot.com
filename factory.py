f1d72ef9288b6ff9cdcb884c35c53c5b0014d705
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-10-29 14:53:58 +0700

    Flask Session & DotEnv

--- factory.py
+++ factory.py
@@ -4,12 +4,15 @@ def create_app():
 
     :return: Flask object
     """
+    from dotenv import dotenv_values
     from flask import Flask
     from flask_minify import minify
-    from config import DevelopmentConfig as Config
+    from flask_session import Session
 
+    config = dotenv_values('.env')
     app = Flask(import_name=__name__)
-    app.config.from_object(obj=Config)
+    app.config.from_object(obj=config)
+    Session(app=app)
     minify(
         app=app,
         caching_limit=3,
