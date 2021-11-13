13266491facff1ce75c8c20cab5a39d927863cf3
Ricky Afdita A P <ricky.a.a.priliandana@sat.co.id>
2021-11-13 22:27:20 +0700

    -

--- static/app/dashboard/index.js
+++ static/app/dashboard/index.js
@@ -365,12 +365,12 @@ let show = function () {
                     ],
                 },
                 body: {
-                    view: "$tb-code", language: "json",
+                    view: "my-editor", language: "json",
                     readOnly: true, value: view_utilities.generate_source_code(),
                 },
             });
             const ui_window_close = ui_window.queryView({name: "close"});
-            const ui_window_code = ui_window.queryView({view: "$tb-code"});
+            const ui_window_code = ui_window.queryView({view: "my-editor"});
 
             ui_window.attachEvent("onShow", function () {
                 ui_master.hideProgress();
--- static/webix/components/monaco/monaco.js
+++ static/webix/components/monaco/monaco.js
@@ -2,7 +2,7 @@
 
 webix.protoUI({
 
-    name: "$tb-code",
+    name: "my-editor",
 
     $init: function (config) {
         config.cdn = "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min";
